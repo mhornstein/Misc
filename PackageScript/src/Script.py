05d99e7cb88bf9237757ccfb4c7b2c39eaa51a9b
mhornstein <mor.h100@gmail.com>
2015-03-20 10:54:16 +0200

    Adding script

---
+++ PackageScript/src/Script.py
@@ -0,0 +1,8 @@
+jarListLoc = "C:\\Users\\mhornstein\\workspace\\Script\\src"
+jarListFileName = "C:\\Users\\mhornstein\\workspace\\Script\\src\\Jars.csv"
+
+def loadJarsUrl():
+    jarsUril = []
+
+if __name__ == '__main__':
+    jarsFiles = loadJarsUrl()
