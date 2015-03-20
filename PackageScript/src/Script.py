75fe2b348abcdd4183fc44bd05b1e0970ae6ab97
mhornstein <mor.h100@gmail.com>
2015-03-20 11:27:43 +0200

    Work in progress

--- PackageScript/src/Script.py
+++ PackageScript/src/Script.py
@@ -1,8 +1,51 @@
-jarListLoc = "C:\\Users\\mhornstein\\workspace\\Script\\src"
-jarListFileName = "C:\\Users\\mhornstein\\workspace\\Script\\src\\Jars.csv"
+import urllib2
+import os
+import zipfile
+import os.path
 
+jarListLoc = "C:\\Users\\mhornstein\\workspace\\Script\\src"
+jarDownloadLoc = "tmp"
+jarListFileName = "Jars2.csv"
+
+def main():
+    generatePackagesName()
+
+def generatePackagesName():
+    jarsLoc = loadJarsUrl()
+    os.chdir(jarDownloadLoc)
+    for jar in jarsLoc:
+        jarName = jar
+        jarUrl = jarsLoc[jarName]
+        
+        downloadFile(jarName, jarUrl)
+        unzipFile(jarName)
+        packagesNames = getPackagesNames()
+        clean()
+        
 def loadJarsUrl():
-    jarsUril = []
+    jarsUrl = {}
+    lines = [line.strip() for line in open(jarListFileName)]
+    for line in lines:
+        jarInfo=line.split(",")
+        jarsUrl[jarInfo[0]] = jarInfo[-1]
+    return jarsUrl
+
+def downloadFile(fileName, url):
+    with open(fileName,'wb') as f:
+        f.write(urllib2.urlopen(url).read())
+        f.close()
+
+def unzipFile(jarName):
+    with zipfile.ZipFile(jarName, "r") as z:
+        z.extractall(".")
+        
+def getPackagesNames():
+    d='.'
+    dirs = [os.path.join(d,o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o))]
+    print dirs
+
+def clean():
+    return None
 
 if __name__ == '__main__':
-    jarsFiles = loadJarsUrl()
+    main()
