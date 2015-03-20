5b139157c319d044a9cb9136f838e0132116900c
mhornstein <mor.h100@gmail.com>
2015-03-20 12:34:27 +0200

    Done

--- PackageScript/src/Script.py
+++ PackageScript/src/Script.py
@@ -1,26 +1,33 @@
 import urllib2
 import os
 import zipfile
-import os.path
+import shutil
 
-jarListLoc = "C:\\Users\\mhornstein\\workspace\\Script\\src"
+'''
+This script:
+(1) Loads jar files downlod adress.
+(2) Downloads the jar
+(3) Generates the package prefix in java imports
+(4) write result to file
+'''
+
 jarDownloadLoc = "tmp"
+doneJarsLoc = "done"
 jarListFileName = "Jars2.csv"
 
 def main():
-    generatePackagesName()
-
-def generatePackagesName():
     jarsLoc = loadJarsUrl()
     os.chdir(jarDownloadLoc)
+    packagesNames = {}
     for jar in jarsLoc:
         jarName = jar
         jarUrl = jarsLoc[jarName]
         
         downloadFile(jarName, jarUrl)
         unzipFile(jarName)
-        packagesNames = getPackagesNames()
-        clean()
+        packagesNames[jarName]= getPackagesNames()
+        clean(jarName)
+    writeRes(packagesNames) 
         
 def loadJarsUrl():
     jarsUrl = {}
@@ -38,14 +45,40 @@ def downloadFile(fileName, url):
 def unzipFile(jarName):
     with zipfile.ZipFile(jarName, "r") as z:
         z.extractall(".")
+        z.close()
         
 def getPackagesNames():
+    def getPackgeName(name):
+        name = name.replace(".\\", "") #removes the initial .\\
+        name = name.replace(".", "") #in case this is the root
+        name = name.replace ("\\", ".") #translate into package structure
+        return name
+        
+    def isValidPackagenName(name):
+        if "META-INF" in name:
+            return False
+        name = getPackgeName(name)
+        if name != None and len(name.split(".")) == 3:
+            return True
+        else:
+            return False
+        
     d='.'
-    dirs = [os.path.join(d,o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o))]
-    print dirs
+    dirs = [getPackgeName(x[0]) for x in os.walk(d) if isValidPackagenName(x[0])]
+    return dirs
+    
 
-def clean():
-    return None
+def clean(jarName):
+    for x in next(os.walk('.'))[1]:
+        shutil.rmtree(x)
+    shutil.move(jarName, "..\\" +doneJarsLoc + "\\" + jarName)
+    
+def writeRes(packagesNames):
+    with open('res.csv', 'a') as f:
+        for jarName in packagesNames:
+            for packageName in packagesNames[jarName]:
+                f.write(jarName + "," + packageName)
+    f.close()
 
 if __name__ == '__main__':
     main()
